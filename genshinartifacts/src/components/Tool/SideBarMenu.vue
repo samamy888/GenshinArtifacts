a6f8eea435215abef5221aeecadcf02e50b6144b
wenhui_xiao <wenhui_xiao@9splay.com>
2021-01-06 17:17:22 +0800

    extend  測試

--- genshinartifacts/src/components/Tool/SideBarMenu.vue
+++ genshinartifacts/src/components/Tool/SideBarMenu.vue
@@ -1,9 +1,17 @@
 <template>
-  <sidebar-menu :menu="menu" :theme="'white-theme'" :collapsed="true" :show-one-child="true" />
+  <sidebar-menu
+    :menu="menu"
+    :theme="'white-theme'"
+    :collapsed="true"
+    :show-one-child="true"
+  />
 </template>
  
 <script>
+const Vue = require('vue').default;
+
 export default {
+
   data() {
     return {
       menu: [
@@ -19,34 +27,47 @@ export default {
             element: "font-awesome-icon",
             attributes: {
               // icon props:
-              icon: 'home',
+              icon: "home",
             },
           },
         },
         {
           href: "/ComparePage",
           title: "原神裝備比對",
-           icon: {
+          icon: {
             element: "font-awesome-icon",
             attributes: {
               // icon props:
-              icon: 'balance-scale',
+              icon: "balance-scale",
             },
           },
         },
         {
           href: "/MaidComparePage",
           title: "女僕裝備比對",
-           icon: {
+          icon: {
             element: "font-awesome-icon",
             attributes: {
               // icon props:
-              icon: 'shield-alt',
+              icon: "shield-alt",
             },
           },
         },
       ],
     };
   },
+  mounted() {
+    
+    setTimeout(() => {
+      let font_comp = Vue.extend("font-awesome-icon");
+      new font_comp({
+        propsData: {
+          icon: "arrows-alt-h",
+        },
+      }).$mount(".vsm--toggle-btn_default");
+    }, 50);
+  },
 };
 </script> 
+<style>
+</style>
